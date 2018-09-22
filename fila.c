23d1d22e00f3383cac2b17c7bb398b4fc79faa04
luizLHS <henriquesaydt2@hotmail.com>
2018-09-22 11:17:05 -0300

    Criação das funções da fila.c

--- fila.c
+++ fila.c
@@ -2,35 +2,79 @@
 #include <stdlib.h>
 #include "fila.h"
 
+
 typedef struct no {
   Token token;
   struct no *prox;
 } No;
 
-struct fila {
+typedef struct fila {
   No *primeiro, *ultimo;
-};
+} Fila;
 
 Fila* fila_criar() {
-	//Implemente
+	Fila *f = (Fila *) malloc(sizeof(Fila));
+    f->primeiro = NULL;
+    f->ultimo = NULL;
+    return f;
 }
 
 void fila_adicionar(Fila *f, Token t) {
-	//Implemente
+	No *tmp = (No *) malloc(sizeof(No));
+    tmp->token = t;
+    tmp->prox = NULL;
+
+    if(f->ultimo == NULL){
+        f->primeiro = tmp;
+        f->ultimo = tmp;
+        return;
+    }
+
+    f->ultimo->prox = tmp;
+    f->ultimo = tmp;
 }
 
 Token fila_remover(Fila *f) {
-	//Implemente
+    if(f->primeiro == NULL){
+        printf("A fila esta vazia!");
+        return;
+    }
+
+    No *tmp;
+    tmp = f->primeiro;
+
+    f->primeiro = f->primeiro->prox;
+
+    if(f->primeiro == NULL){
+        f->ultimo == NULL;
+    }
+
+    tmpToken = tmp->token;
+    free(tmp)
+
+    return(tmpToken);
 }
 
 int fila_vazia(Fila *f) {
-	//Implemente
+	if(f->primeiro == NULL){
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
 
 void fila_destruir(Fila *f) {
-	//Implemente
+	free(f);
 }
 
 void fila_imprimir(Fila *f) {
-	//Implemente
+	No *tmp;
+    tmp = f->primeiro;
+
+    while(tmp != NULL){
+        token_imprimir(tmp->token);
+        tmp = tmp->prox;
+    }
+
 }
